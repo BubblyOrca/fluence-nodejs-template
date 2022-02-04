80c58e989b85f32b7e2ee7239be8c3a4e2e24f10
Pavel Murygin <pavel.murygin@gmail.com>
2022-02-05 00:43:24 +0300

    impl

---
+++ send_messge.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+npx aqua run --input ./aqua/export.aqua --func 'sendEveryone("hackathon", "hello, world!")' --addr /dns4/stage.fluence.dev/tcp/19001/wss/p2p/12D3KooWHCJbJKGDfCgHSoCuK9q4STyRnVveqLoXAPBbXHTZx9Cv 
+# npx aqua run --input ./aqua/export.aqua --func 'sendMyself("12D3KooWMJMp17Q6GR54AkLc3EUkVinLS9YQUTawvs7QVJKzuQYw", "12D3KooWHLxVhUQyAuZe6AHMB29P7wkvTNMn7eDMcsqimJYLKREf", "hello, world11!")' --addr /dns4/kras-00.fluence.dev/tcp/19001/wss/p2p/12D3KooWR4cv1a8tv7pps4HH6wePNaK6gf1Hww5wcCMzeWxyNw51
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,40 @@
-import { Fluence } from '@fluencelabs/fluence';
+import { Fluence, KeyPair } from '@fluencelabs/fluence';
 import { krasnodar } from '@fluencelabs/fluence-network-environment';
-import { getRelayTimestamp } from './_aqua/export';
+import { advertiseMyself, registerMessagingService, sendMyself } from './_aqua/export';
 
 const relay = krasnodar[3];
 
+const sk = Buffer.from('SVz4T4yW718wt0rziDVOfiv6+WQbS4lvEtJHEieXcAk=', 'base64');
+
 async function main() {
     await Fluence.start({
+        KeyPair: await KeyPair.fromEd25519SK(sk),
         connectTo: relay,
     });
 
-    const timestamp = await getRelayTimestamp();
-    console.log(new Date(timestamp));
+    console.log('own peer id ', Fluence.getStatus().peerId);
+    console.log('connected to ', Fluence.getStatus().relayPeerId);
 
-    await Fluence.stop();
+    registerMessagingService({
+        receiveMessage: (str) => {
+            console.log(str);
+        },
+    });
+
+    console.log('messaging service registered');
+
+    await advertiseMyself('hackathon');
+
+    console.log('messaging service advertised');
+
+    console.log('press any key to quit...');
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', async () => {
+        await Fluence.stop();
+        process.exit(0);
+    });
 }
 
 main().catch((err) => console.error(err));
